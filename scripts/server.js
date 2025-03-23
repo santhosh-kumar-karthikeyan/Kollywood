9f486818519c193d1da42def450898bf2bf8c396
TheMadTitan2005 <goonpathy>
2025-03-23 16:37:53 +0530

    added logic for validating login, but only for consoling loggin in validity. To add GUI response.

--- scripts/server.js
+++ scripts/server.js
@@ -53,7 +53,36 @@ app.post("/validateSignup",async (req,res) => {
     console.log("User added");
     res.sendFile(path.join(__dirname,"../html/index.html"));
 });
-app.verify
+app.post("/validateLogin",async (req,res) => {
+    console.log("checking login presence..");
+    const {username, password} = req.body;
+    const foundUser = await users.findOne({username: username});
+    console.log(foundUser.pass);
+    if(foundUser) {
+        if(foundUser.pass.startsWith("$")) {
+            try {
+                if(await argon.verify(foundUser.pass, password)) {
+                    console.log("Correct password!!");
+                    return res.json({validity: true, userFound: true});
+                }
+                else    
+                    console.log("Incorrect password!!");
+                    res.sendFile(path.join(__dirname,"../html/login.html"));
+                    return res.json({validity: false, userFound: true});
+            }
+            catch(err) {
+                console.error("Can't verify hash...");
+            }
+        }
+        else {
+            console.log("Stored hash is invalid");
+        }
+    }
+    else {
+        console.log("User doesn't exist");
+        return res.json({validity: false, userFound: false});
+    }
+});
 app.post("/checkUsername",async (req,res) => {
     console.log("checking username");
     const {username} = req.body;
